bc164919448260de7d4a06aeb8a088ef52685851
Sergio Carvalho <a89313@alunos.uminho.pt>
2020-10-30 17:23:59 +0000

    added new features

--- app/src/Communication.java
+++ app/src/Communication.java
@@ -1,8 +1,12 @@
 import com.fazecast.jSerialComm.SerialPort;
 import com.fazecast.jSerialComm.SerialPortDataListener;
 import com.fazecast.jSerialComm.SerialPortEvent;
+import org.apache.commons.io.FileUtils;
 
+import java.io.File;
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -10,13 +14,21 @@ import java.util.Scanner;
 public class Communication {
 
     private SerialPort serialPort;
-    private StringBuilder message = new StringBuilder();
-    private byte[] accent = new byte[2];
-    private boolean founded = false;
+    private StringBuilder message = new StringBuilder();  //received message
+    private byte[] accent = new byte[2]; // array to fix accent on strings
+    private boolean founded = false; // flag if a accent is founded
+    private int fileBytesSize = 2747; //hard coded file size
+    private int counterBytes = 0; //actual readed bytes
+    private byte[] receivedFile = new byte[fileBytesSize]; //received file
     private static Communication instance = new Communication();
 
     public Communication() {
+
         serialPort = chooseSerialPort();
+        while (serialPort == null){
+            System.out.println("Choose a existing port");
+            serialPort = chooseSerialPort();
+        }
         serialPort.openPort();
         checkConnection();
         setEventHandler();
@@ -27,19 +39,20 @@ public class Communication {
     }
 
 
-
-    private SerialPort chooseSerialPort(){
+    private SerialPort chooseSerialPort() {
         System.out.println(Arrays.toString(SerialPort.getCommPorts()));
         int i = 0;
         System.out.println("Escolhe a porta que está conectada ao teu Arduino");
-        for(SerialPort port : SerialPort.getCommPorts()){
+        for (SerialPort port : SerialPort.getCommPorts()) {
             System.out.println(i + "- " + port.getSystemPortName());
             i++;
         }
         Scanner option = new Scanner(System.in);
         int serial = Integer.parseInt(option.nextLine());
-        return SerialPort.getCommPorts()[serial];
-
+        if(serial > -1 && serial < i)
+            return SerialPort.getCommPorts()[serial];
+        else
+            return null;
     }
 
     private void checkConnection() {
@@ -110,22 +123,41 @@ public class Communication {
                 byte[] newData = new byte[serialPort.bytesAvailable()];
                 serialPort.readBytes(newData, newData.length);
 
-                for (byte b : newData) {
-                    if ((char) b != '\n') {
-                        if ((int) b < 0 && !founded) {
-                            fixReceivedByte(0, b);
-                            continue;
-                        }
-                        if (founded) {
-                            fixReceivedByte(1, b);
-                            continue;
-                        }
-                        char letter = (char) b;
-                        message.append(letter);
+                if (newData[0] == 0) {
+                    // if first byte is 0 is string, 1 is file
+                    for (byte b : newData) {
+                        if ((char) b != '\n') {
+                            if ((int) b < 0 && !founded) {
+                                fixReceivedByte(0, b);
+                                continue;
+                            }
+                            if (founded) {
+                                fixReceivedByte(1, b);
+                                continue;
+                            }
+                            char letter = (char) b;
+                            message.append(letter);
 
-                    } else {
-                        System.out.println("Outro:" + message.toString());
-                        message = new StringBuilder();
+                        } else {
+                            System.out.println("Outro:" + message.toString());
+                            message = new StringBuilder();
+                        }
+                    }
+                } else {
+                    for (int j = 0; j < newData.length; j++) {
+
+                        if (j != 0) {
+                            byte b = newData[j];
+
+                            if (counterBytes != fileBytesSize) {
+                                receivedFile[counterBytes] = b;
+                                counterBytes++;
+                            }
+                            else{
+                                saveFile();
+                            }
+                        }
+
                     }
                 }
 
@@ -136,20 +168,22 @@ public class Communication {
 
     /**
      * @param message - the input message from user
-     *
-     *  in this method the inputed string from user is converter to a byte array to send via serial port
-     *  to the arduino
+     *                <p>
+     *                in this method the inputed string from user is converter to a byte array to send via serial port
+     *                to the arduino
      */
     public void stringToByte(String message) {
 
         int messageSize = checkStringSize(message);
-        byte[] bArray = new byte[messageSize + 2];  //plus 2 to handle the end chars
+        byte[] bArray = new byte[messageSize + 3];  //plus 2 to handle the end chars
 
         int counter = 0;
 
-        for (int i = 0; i < messageSize; i++) {
+        bArray[0] = 0;
+
+        for (int i = 1; i < messageSize; i++) {
             //check if is a character with accent
-            if ( (byte) message.charAt(counter) > 0 )
+            if ((byte) message.charAt(counter) > 0)
                 bArray[i] = (byte) message.charAt(counter);
             else {
                 byte[] fixed = fixByteToSend((byte) message.charAt(counter));
@@ -185,6 +219,55 @@ public class Communication {
 
     }
 
+    /**
+     * method to build the packet of file
+     */
+
+    public void sendFile() {
+        byte[] fileBytes = readFile();
+
+        if (fileBytes != null) {
+            byte[] fileToSend = new byte[fileBytes.length + 3];
+            int i = 0;
+            fileToSend[i] = 1;
+            i++;
+            for (byte b : fileBytes) {
+                fileToSend[i] = b;
+                i++;
+            }
+
+            fileToSend[fileToSend.length - 2] = '\n';
+            fileToSend[fileToSend.length - 1] = '\b';
+
+            serialPort.writeBytes(fileToSend, fileToSend.length);
+        }
+    }
+
+    /**
+     *
+     * @return array of bytes from file
+     */
+    private byte[] readFile() {
+        //TODO: read path from terminal
+        File toRead = new File("Dados.ppI");
+        try {
+            return Files.readAllBytes(toRead.toPath());
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            return null;
+        }
+    }
+
+    private void saveFile() {
+        try {
+            FileUtils.writeByteArrayToFile(new File("DadosRecebidos.ppI"), receivedFile);
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        System.out.println("File readed with sucess!");
+        receivedFile = new byte[fileBytesSize];
+        counterBytes = 0;
+    }
 
     public String readUserMessage() {
         Scanner input = new Scanner(System.in);
--- app/src/ProgramController.java
+++ app/src/ProgramController.java
@@ -3,6 +3,7 @@ public class ProgramController {
 
     public static void main(String[] args) {
         Communication communication = Communication.getInstance();
+        communication.sendFile();
         while (true){
             communication.stringToByte(communication.readUserMessage());
         }
